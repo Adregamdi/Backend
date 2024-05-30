19a295f9b65ffae4d6cd890f376e992eb631ef34
fakerdeft <whakswp123@naver.com>
2024-05-30 22:49:02 +0900

    chore: 초기 세팅

--- src/main/java/com/adregamdi/AdregamdiApplication.java
+++ src/main/java/com/adregamdi/AdregamdiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AdregamdiBackendApplication {
+public class AdregamdiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AdregamdiBackendApplication.class, args);
+        SpringApplication.run(AdregamdiApplication.class, args);
     }
 
 }

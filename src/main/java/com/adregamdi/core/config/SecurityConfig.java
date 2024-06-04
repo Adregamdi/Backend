c4d1001f0fcaf8ccac5e89320cbc47c59b4d353b
fakerdeft <whakswp123@naver.com>
2024-06-04 23:41:19 +0900

    (#5)feat: 시큐리티 config 구현 중

--- src/main/java/com/adregamdi/core/config/SecurityConfig.java
+++ src/main/java/com/adregamdi/core/config/SecurityConfig.java
@@ -1,9 +1,16 @@
 package com.adregamdi.core.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
 @RequiredArgsConstructor
 @Configuration
@@ -15,4 +22,32 @@ public class SecurityConfig {
             "/",
             "/index.html"
     };
+
+    @Bean
+    public SecurityFilterChain filterChain(
+            final HttpSecurity http
+    ) throws Exception {
+        http
+                .cors(Customizer.withDefaults())
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+//                .headers(AbstractHttpConfigurer::disable)
+                .headers(headers ->
+                        headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(request ->
+                        request.requestMatchers(ALLOWED_URLS).permitAll()
+                                .anyRequest().authenticated())
+//                .oauth2Login(oauth2Login -> oauth2Login
+//                        .userInfoEndpoint(userInfo -> userInfo.userService(customOAuth2UserService))
+//                        .successHandler(oAuth2LoginSuccessHandler) // 동의하기 눌렀을 때 핸들러 설정
+//                        .failureHandler(oAuth2LoginFailureHandler) // 소셜 로그인 실패 시 핸들러 설정
+//                )
+        ;
+
+        return http.build();
+    }
 }

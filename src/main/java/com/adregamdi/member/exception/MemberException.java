4502af6fcf24863761464025488efabc10c38566
fakerdeft <whakswp123@naver.com>
2024-06-12 23:40:19 +0900

    (#5)feat: 회원 예외 구현

---
+++ src/main/java/com/adregamdi/member/exception/MemberException.java
@@ -0,0 +1,33 @@
+package com.adregamdi.member.exception;
+
+public class MemberException extends RuntimeException {
+
+    public MemberException(final String message) {
+        super(message);
+    }
+
+    public static class MemberNotFoundException extends MemberException {
+
+        public MemberNotFoundException() {
+            super("회원이 존재하지 않습니다.");
+        }
+
+        public MemberNotFoundException(final Object data) {
+            super(String.format("회원이 존재하지 않습니다. - request info => %s", data));
+        }
+    }
+
+    public static class AllMemberNotFoundException extends MemberException {
+
+        public AllMemberNotFoundException() {
+            super("모든 회원이 존재하지 않습니다.");
+        }
+    }
+
+    public static class SuspendedMembershipException extends MemberException {
+
+        public SuspendedMembershipException(final Object data) {
+            super(String.format("유예 상태인 회원은 가입이 불가능합니다. - request info => %s", data));
+        }
+    }
+}

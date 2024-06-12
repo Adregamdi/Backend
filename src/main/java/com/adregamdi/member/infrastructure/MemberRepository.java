f2bcb6248e2d3dcecddc59e46666e62eb82750ce
fakerdeft <whakswp123@naver.com>
2024-06-12 23:39:56 +0900

    (#5)fix: 상속 제네릭 위치 수정

--- src/main/java/com/adregamdi/member/infrastructure/MemberRepository.java
+++ src/main/java/com/adregamdi/member/infrastructure/MemberRepository.java
@@ -3,5 +3,5 @@ package com.adregamdi.member.infrastructure;
 import com.adregamdi.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<Long, Member> {
+public interface MemberRepository extends JpaRepository<Member, Long> {
 }

93e78222087e71323526b1bdce751a8f818e7e2c
fakerdeft <whakswp123@naver.com>
2024-06-12 23:40:13 +0900

    (#5)feat: 회원 권한 구현

---
+++ src/main/java/com/adregamdi/member/domain/Role.java
@@ -0,0 +1,15 @@
+package com.adregamdi.member.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+
+    GUEST("GUEST"),
+    MEMBER("MEMBER"),
+    ADMIN("ADMIN");
+
+    private final String key;
+}

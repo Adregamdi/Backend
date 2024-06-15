2dc9d7ce5d54d331686c8a39f84105843e8c1e98
fakerdeft <whakswp123@naver.com>
2024-06-15 22:42:48 +0900

    (#5)feat: 시큐리티 환경 설정 구현

--- src/main/java/com/adregamdi/core/config/SecurityConfig.java
+++ src/main/java/com/adregamdi/core/config/SecurityConfig.java
@@ -1,5 +1,11 @@
 package com.adregamdi.core.config;
 
+import com.adregamdi.core.jwt.filter.JwtAuthenticationFilter;
+import com.adregamdi.core.jwt.service.JwtService;
+import com.adregamdi.core.oauth.handler.OAuth2LoginFailureHandler;
+import com.adregamdi.core.oauth.handler.OAuth2LoginSuccessHandler;
+import com.adregamdi.core.oauth.service.CustomOAuth2UserService;
+import com.adregamdi.member.infrastructure.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,13 +17,18 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
 
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
-
+    private final JwtService jwtService;
+    private final MemberRepository memberRepository;
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
+    private final OAuth2LoginFailureHandler oAuth2LoginFailureHandler;
     private final String[] ALLOWED_URLS = {
             "/",
             "/index.html"
@@ -41,13 +52,20 @@ public class SecurityConfig {
                 .authorizeHttpRequests(request ->
                         request.requestMatchers(ALLOWED_URLS).permitAll()
                                 .anyRequest().authenticated())
-//                .oauth2Login(oauth2Login -> oauth2Login
-//                        .userInfoEndpoint(userInfo -> userInfo.userService(customOAuth2UserService))
-//                        .successHandler(oAuth2LoginSuccessHandler) // 동의하기 눌렀을 때 핸들러 설정
-//                        .failureHandler(oAuth2LoginFailureHandler) // 소셜 로그인 실패 시 핸들러 설정
-//                )
+                .oauth2Login(oauth2Login -> oauth2Login
+                        .userInfoEndpoint(userInfo -> userInfo.userService(customOAuth2UserService))
+                        .successHandler(oAuth2LoginSuccessHandler) // 동의하기 눌렀을 때 핸들러 설정
+                        .failureHandler(oAuth2LoginFailureHandler) // 소셜 로그인 실패 시 핸들러 설정
+                )
+                .addFilterAfter(jwtAuthenticationFilter(), LogoutFilter.class)
         ;
 
         return http.build();
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter(
+    ) {
+        return new JwtAuthenticationFilter(jwtService, memberRepository);
     }
 }

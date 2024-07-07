92fb9bf2f8fdac98ff1ee1e8b6036f3e7d92a84f
UncleSubak <kko6369@naver.com>
2024-07-07 17:24:30 +0900

    [#17] Chore : 2024-07-07 `@SpringBootTest` 임시 주석

--- src/test/java/com/adregamdi/AdregamdiApplicationTests.java
+++ src/test/java/com/adregamdi/AdregamdiApplicationTests.java
@@ -3,7 +3,7 @@ package com.adregamdi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class AdregamdiApplicationTests {
 
     @Test

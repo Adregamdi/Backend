359cb284e8cd7f882a0b125925d311e347b77e89
fakerdeft <whakswp123@naver.com>
2024-07-12 02:02:12 +0900

    (#15)feat: 로그아웃 구현

--- src/main/java/com/adregamdi/member/presentation/MemberController.java
+++ src/main/java/com/adregamdi/member/presentation/MemberController.java
@@ -1,6 +1,14 @@
 package com.adregamdi.member.presentation;
 
+import com.adregamdi.core.annotation.MemberAuthorize;
+import com.adregamdi.core.handler.ApiResponse;
+import com.adregamdi.member.application.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/member")
 @RestController
 public class MemberController {
+    private final MemberService memberService;
+
+    @PostMapping("/logout")
+    @MemberAuthorize
+    public ResponseEntity<ApiResponse<Void>> logout(@AuthenticationPrincipal final UserDetails userDetails) {
+        memberService.logout(userDetails.getUsername());
+        return ResponseEntity.ok()
+                .body(ApiResponse.<Void>builder()
+                        .statusCode(HttpStatus.OK)
+                        .build());
+    }
 }

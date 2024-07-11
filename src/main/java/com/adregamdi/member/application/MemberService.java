98a9b0f998c65d01cdd2210daa3c49f3ad753684
fakerdeft <whakswp123@naver.com>
2024-07-12 02:02:16 +0900

    (#15)feat: 로그아웃 구현

--- src/main/java/com/adregamdi/member/application/MemberService.java
+++ src/main/java/com/adregamdi/member/application/MemberService.java
@@ -1,11 +1,26 @@
 package com.adregamdi.member.application;
 
+import com.adregamdi.member.domain.Member;
+import com.adregamdi.member.exception.MemberException.MemberNotFoundException;
+import com.adregamdi.member.infrastructure.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class MemberService {
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public void logout(final String memberId) {
+        Member member = memberRepository.findByIdAndMemberStatus(UUID.fromString(memberId), true)
+                .orElseThrow(() -> new MemberNotFoundException(memberId));
+
+        member.updateRefreshTokenStatus(false);
+    }
 }

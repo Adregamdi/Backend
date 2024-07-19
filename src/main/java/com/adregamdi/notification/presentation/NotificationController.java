5b84a9c84ed8ac6bab84639c34b453cc26054494
fakerdeft <whakswp123@naver.com>
2024-07-20 01:32:30 +0900

    (#22)feat: 알림 컨트롤러 생성

---
+++ src/main/java/com/adregamdi/notification/presentation/NotificationController.java
@@ -0,0 +1,11 @@
+package com.adregamdi.notification.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/notification")
+@RestController
+public class NotificationController {
+}

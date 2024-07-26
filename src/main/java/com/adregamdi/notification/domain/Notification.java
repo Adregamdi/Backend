b106cfd31777c41eeb8aaa9fb12f2709b6af48a2
fakerdeft <whakswp123@naver.com>
2024-07-26 17:27:41 +0900

    (#22)chore: 주석 추가

--- src/main/java/com/adregamdi/notification/domain/Notification.java
+++ src/main/java/com/adregamdi/notification/domain/Notification.java
@@ -26,7 +26,7 @@ public class Notification extends BaseTime {
     private boolean isRead; // 상태 (T: 읽음, F: 안 읽음)
 
     @Enumerated(EnumType.STRING)
-    private NotificationType type; // 알림 종류 ()
+    private NotificationType type; // 알림 종류 (좋아요, 일정)
 
     public void updateIsRead(final boolean isRead) {
         this.isRead = isRead;

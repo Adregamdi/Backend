b6c93ac45014845b4bbf247d486e13e957324fc7
fakerdeft <whakswp123@naver.com>
2024-07-26 16:40:50 +0900

    (#22)feat: 알림 조회 응답 구현

--- src/main/java/com/adregamdi/notification/dto/response/GetNotificationResponse.java
+++ src/main/java/com/adregamdi/notification/dto/response/GetNotificationResponse.java
@@ -1,6 +1,19 @@
 package com.adregamdi.notification.dto.response;
 
-public record GetNotificationResponse(
+import com.adregamdi.notification.dto.NotificationDTO;
+import lombok.Builder;
 
+import java.util.List;
+
+@Builder
+public record GetNotificationResponse(
+        int noReadElements,
+        List<NotificationDTO> notificationList
 ) {
+    public static GetNotificationResponse of(final int noReadElements, final List<NotificationDTO> notificationList) {
+        return GetNotificationResponse.builder()
+                .noReadElements(noReadElements)
+                .notificationList(notificationList)
+                .build();
+    }
 }

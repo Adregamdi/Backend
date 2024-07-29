3b18dbd2b696c026cbdf996a77c631e9c51f5d67
fakerdeft <whakswp123@naver.com>
2024-07-29 20:38:08 +0900

    (#27)feat: 일정 컨트롤러 생성

---
+++ src/main/java/com/adregamdi/schedule/presentation/ScheduleController.java
@@ -0,0 +1,11 @@
+package com.adregamdi.schedule.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/schedule")
+@RestController
+public class ScheduleController {
+}

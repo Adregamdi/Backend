8ecb286551fe9bb4316f10980a18ff9576403858
fakerdeft <whakswp123@naver.com>
2024-08-06 00:32:07 +0900

    (#27)feat: 장소 조회 메서드 구현

--- src/main/java/com/adregamdi/place/presentation/PlaceController.java
+++ src/main/java/com/adregamdi/place/presentation/PlaceController.java
@@ -1,8 +1,16 @@
 package com.adregamdi.place.presentation;
 
+import com.adregamdi.core.annotation.MemberAuthorize;
+import com.adregamdi.core.handler.ApiResponse;
 import com.adregamdi.place.application.PlaceService;
+import com.adregamdi.place.dto.response.GetPlaceResponse;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -10,4 +18,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PlaceController {
     private final PlaceService placeService;
+
+    @GetMapping
+    @MemberAuthorize
+    public ResponseEntity<ApiResponse<GetPlaceResponse>> get(
+            @RequestParam @PositiveOrZero final int pageNo,
+            @RequestParam final String name
+    ) {
+        return ResponseEntity.ok()
+                .body(ApiResponse.<GetPlaceResponse>builder()
+                        .statusCode(HttpStatus.OK)
+                        .data(placeService.get(pageNo, name))
+                        .build()
+                );
+    }
 }

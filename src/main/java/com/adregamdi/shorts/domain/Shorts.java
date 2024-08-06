ab0af9ac80a63b8a0e4d47fcf14ab2a3e5b58ff5
wanny <115992753+Subak-Uncle@users.noreply.github.com>
2024-08-06 13:09:59 +0900

    [#28] Feat : 2024-08-06 Shorts 도메인 엔티티 설계

---
+++ src/main/java/com/adregamdi/shorts/domain/Shorts.java
@@ -0,0 +1,44 @@
+package com.adregamdi.shorts.domain;
+
+import com.adregamdi.core.entity.BaseTime;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.Comment;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "tbl_shorts")
+public class Shorts extends BaseTime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    @Comment(value = "쇼츠 제목")
+    private String title;
+
+    @Column
+    @Comment(value = "작성자")
+    private UUID memberId;
+
+    @Column
+    @Comment(value = "장소")
+    private Long placeNo;
+
+    @Column
+    @Comment(value = "여행기 번호")
+    private Long travelReviewNo;
+
+    @Builder
+    public Shorts(Long id, String title, UUID memberId, Long placeNo, Long travelReviewNo) {
+        this.id = id;
+        this.title = title;
+        this.memberId = memberId;
+        this.placeNo = placeNo;
+        this.travelReviewNo = travelReviewNo;
+    }
+}

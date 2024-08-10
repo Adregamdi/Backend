24d5f58945f49137a0ce34c5b19f4a44d9c355ea
fakerdeft <whakswp123@naver.com>
2024-08-10 23:47:14 +0900

    (#29)refactor: 로그인 메서드 생성

--- src/main/java/com/adregamdi/core/oauth2/presentation/OAuth2Controller.java
+++ src/main/java/com/adregamdi/core/oauth2/presentation/OAuth2Controller.java
@@ -1,18 +1,14 @@
 package com.adregamdi.core.oauth2.presentation;
 
 import com.adregamdi.core.handler.ApiResponse;
-import com.adregamdi.core.jwt.service.JwtService;
 import com.adregamdi.core.oauth2.service.OAuth2Service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/oauth2/kakao")

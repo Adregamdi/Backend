94eca24cfbe7f6648501c426d92f7093ce497035
fakerdeft <whakswp123@naver.com>
2024-08-16 00:54:01 +0900

    (#27)feat: 장소 검색 기능 구현

--- src/main/java/com/adregamdi/place/application/PlaceServiceImpl.java
+++ src/main/java/com/adregamdi/place/application/PlaceServiceImpl.java
@@ -1,19 +1,36 @@
 package com.adregamdi.place.application;
 
+import com.adregamdi.place.domain.Place;
+import com.adregamdi.place.dto.PlaceDTO;
 import com.adregamdi.place.dto.response.GetPlaceResponse;
+import com.adregamdi.place.exception.PlaceException.PlaceNotFoundException;
+import com.adregamdi.place.infrastructure.PlaceRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static com.adregamdi.core.constant.Constant.NORMAL_PAGE_SIZE;
+import static com.adregamdi.core.utils.PageUtil.generatePageAsc;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class PlaceServiceImpl implements PlaceService {
+    private final PlaceRepository placeRepository;
+
     @Override
     @Transactional(readOnly = true)
     public GetPlaceResponse get(int pageNo, String name) {
-        return null;
+        Slice<Place> places = placeRepository.findByNameStartingWith(generatePageAsc(pageNo, NORMAL_PAGE_SIZE, "name"), name)
+                .orElseThrow(() -> new PlaceNotFoundException(name));
+        return GetPlaceResponse.from(
+                places.getContent()
+                        .stream()
+                        .map(PlaceDTO::from)
+                        .toList()
+        );
     }
 
     @Override

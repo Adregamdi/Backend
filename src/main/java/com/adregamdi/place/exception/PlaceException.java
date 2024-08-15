458f5f3e9844b0430854b46af18eccde86facb5b
fakerdeft <whakswp123@naver.com>
2024-08-15 22:01:58 +0900

    (#27)feat: 장소 not found 예외 추가

--- src/main/java/com/adregamdi/place/exception/PlaceException.java
+++ src/main/java/com/adregamdi/place/exception/PlaceException.java
@@ -1,4 +1,18 @@
 package com.adregamdi.place.exception;
 
 public class PlaceException extends RuntimeException {
+    public PlaceException(final String message) {
+        super(message);
+    }
+
+    public static class PlaceNotFoundException extends PlaceException {
+
+        public PlaceNotFoundException() {
+            super("장소가 존재하지 않습니다.");
+        }
+
+        public PlaceNotFoundException(final Object object) {
+            super(String.format("장소가 존재하지 않습니다. - request info => %s", object));
+        }
+    }
 }

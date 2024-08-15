4d767f7e17342db538bf1eb5a906ef622e56c261
fakerdeft <whakswp123@naver.com>
2024-08-16 00:53:43 +0900

    (#27)feat: 장소 응답 dto 구현

--- src/main/java/com/adregamdi/place/dto/response/GetPlaceResponse.java
+++ src/main/java/com/adregamdi/place/dto/response/GetPlaceResponse.java
@@ -1,4 +1,17 @@
 package com.adregamdi.place.dto.response;
 
-public record GetPlaceResponse() {
+import com.adregamdi.place.dto.PlaceDTO;
+import lombok.Builder;
+
+import java.util.List;
+
+@Builder
+public record GetPlaceResponse(
+        List<PlaceDTO> companyList
+) {
+    public static GetPlaceResponse from(final List<PlaceDTO> companyList) {
+        return GetPlaceResponse.builder()
+                .companyList(companyList)
+                .build();
+    }
 }

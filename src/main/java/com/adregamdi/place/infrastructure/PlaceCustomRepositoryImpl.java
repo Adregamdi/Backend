c55d7866988530cd2298adc50f1580d5e33861f0
fakerdeft <whakswp123@naver.com>
2024-08-19 23:40:13 +0900

    (#27)feat: 장소 리스트 검색 기능 구현

--- src/main/java/com/adregamdi/place/infrastructure/PlaceCustomRepositoryImpl.java
+++ src/main/java/com/adregamdi/place/infrastructure/PlaceCustomRepositoryImpl.java
@@ -1,14 +1,33 @@
 package com.adregamdi.place.infrastructure;
 
 import com.adregamdi.place.domain.Place;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
+import static com.adregamdi.core.utils.RepositoryUtil.makeOrderSpecifiers;
+import static com.adregamdi.place.domain.QPlace.place;
+
+@Repository
+@RequiredArgsConstructor
 public class PlaceCustomRepositoryImpl implements PlaceCustomRepository {
+    private final JPAQueryFactory jpaQueryFactory;
+
     @Override
     public Optional<Slice<Place>> findByNameStartingWith(Pageable pageable, String name) {
-        return Optional.empty();
+        List<Place> places = jpaQueryFactory
+                .selectFrom(place)
+                .where(place.name.startsWith(name))
+                .orderBy(makeOrderSpecifiers(place, pageable))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+        return Optional.of(new SliceImpl<>(places));
     }
 }

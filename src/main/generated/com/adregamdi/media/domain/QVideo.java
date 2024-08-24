69772c41184c077369cfb11ab9d4b924e12ed07e
UncleSubak <kko6369@naver.com>
2024-08-25 00:17:18 +0900

    [#28][Feat] VideoService 기능 구현

---
+++ src/main/generated/com/adregamdi/media/domain/QVideo.java
@@ -0,0 +1,45 @@
+package com.adregamdi.media.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QVideo is a Querydsl query type for Video
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QVideo extends EntityPathBase<Video> {
+
+    private static final long serialVersionUID = 292273268L;
+
+    public static final QVideo video = new QVideo("video");
+
+    public final DateTimePath<java.time.LocalDateTime> createAt = createDateTime("createAt", java.time.LocalDateTime.class);
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final EnumPath<com.adregamdi.media.enumtype.MediaType> mediaType = createEnum("mediaType", com.adregamdi.media.enumtype.MediaType.class);
+
+    public final NumberPath<Long> targetId = createNumber("targetId", Long.class);
+
+    public final StringPath url = createString("url");
+
+    public QVideo(String variable) {
+        super(Video.class, forVariable(variable));
+    }
+
+    public QVideo(Path<? extends Video> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QVideo(PathMetadata metadata) {
+        super(Video.class, metadata);
+    }
+
+}
+
--- src/main/java/com/adregamdi/media/application/VideoService.java
+++ src/main/java/com/adregamdi/media/application/VideoService.java
@@ -1,7 +1,9 @@
 package com.adregamdi.media.application;
 
 import com.adregamdi.core.service.FileUploadService;
+import com.adregamdi.media.dto.response.UploadVideoResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.bytedeco.javacv.FFmpegFrameGrabber;
 import org.bytedeco.javacv.Frame;
 import org.bytedeco.javacv.Java2DFrameConverter;
@@ -24,18 +26,26 @@ import java.io.IOException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.util.UUID;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
-public class VideoService implements MediaService{
+public class VideoService {
 
-    private FileUploadService fileUploadService;
+    private final FileUploadService fileUploadService;
+    private final VideoValidateService videoValidateService;
 
     private final static int VIDEO_MAX_BITRATE = 1000000; // 1Mbps
     private final static int VIDEO_MAX_DURATION = 60; // 60 seconds
 
     public String getEncodedFileName(String key) {
-        return URLEncoder.encode(key, StandardCharsets.UTF_8).replaceAll("\\+", "%20");
+        try {
+            return URLEncoder.encode(key, StandardCharsets.UTF_8).replaceAll("\\+", "%20");
+        } catch (Exception e) {
+            log.error("Failed to encode file name: {}", key, e);
+            throw new RuntimeException("파일 인코딩 과정에서 에러가 발생하였습니다.", e);
+        }
     }
 
     public byte[] compressVideo(MultipartFile videoFile) throws IOException, EncoderException {
@@ -43,7 +53,6 @@ public class VideoService implements MediaService{
         File target = File.createTempFile("compressed", "." + fileUploadService.extractFileExtension(videoFile));
 
         MultimediaObject multimediaObject = new MultimediaObject(source);
-        // 동영상 파일 정보 추출
         MultimediaInfo info = multimediaObject.getInfo();
 
         if (info.getDuration() > VIDEO_MAX_DURATION * 1000) {
@@ -51,15 +60,15 @@ public class VideoService implements MediaService{
         }
 
         AudioAttributes audio = new AudioAttributes();
-        audio.setCodec("aac");  // AAC 코덱
-        audio.setBitRate(128000);   // 128 kbps
-        audio.setChannels(2);   // 스테레오 채널
-        audio.setSamplingRate(44100);   // 44.1 kHz 샘플링
+        audio.setCodec("aac");
+        audio.setBitRate(128000);
+        audio.setChannels(2);
+        audio.setSamplingRate(44100);
 
         VideoAttributes video = new VideoAttributes();
         video.setCodec("h264");
         video.setBitRate(VIDEO_MAX_BITRATE);
-        video.setFrameRate(30); // 30 프레임 속도로 인코딩
+        video.setFrameRate(30);
 
         EncodingAttributes attrs = new EncodingAttributes();
         attrs.setOutputFormat("mp4");
@@ -69,15 +78,16 @@ public class VideoService implements MediaService{
         Encoder encoder = new Encoder();
         encoder.encode(multimediaObject, target, attrs);
 
-        byte[] compressedVideo = Files.readAllBytes(target.toPath());
-
         source.delete();
         target.delete();
 
-        return compressedVideo;
+        log.info("성공적으로 비디오가 압축되었습니다.: {}", videoFile.getOriginalFilename());
+        return Files.readAllBytes(target.toPath());
+
+
     }
 
-    public byte[] generateThumbnail(byte[] videoBytes) throws IOException {
+    public byte[] generateThumbnail(byte[] videoBytes) {
         try (ByteArrayInputStream inputStream = new ByteArrayInputStream(videoBytes);
              FFmpegFrameGrabber frameGrabber = new FFmpegFrameGrabber(inputStream)) {
 
@@ -89,7 +99,42 @@ public class VideoService implements MediaService{
 
             ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
             ImageIO.write(thumbnail, "png", outputStream);
+            log.info("성공적으로 썸네일이 제작되었습니다.");
             return outputStream.toByteArray();
+        } catch (IOException e) {
+            log.error("썸네일 제작 중 에러가 발생하였습니다.", e);
+            throw new RuntimeException("썸네일 제작 과정에서 에러가 발생하였습니다.", e);
         }
     }
+
+    public UploadVideoResponse uploadVideo(MultipartFile videoFile, UUID memberId) throws EncoderException {
+        try {
+            log.info("비디오 업로드를 시작합니다. memberId: {}", memberId);
+            videoValidateService.checkVideoFile(videoFile.getOriginalFilename());
+
+            byte[] compressedVideo = compressVideo(videoFile);
+            byte[] thumbnail = generateThumbnail(compressedVideo);
+
+            String videoKey = buildKey(videoFile, memberId.toString(), "shorts");
+            String thumbnailKey = buildKey(videoFile, memberId.toString(), "thumbnails");
+
+            String uploadedVideoUrl = fileUploadService.uploadFile(compressedVideo, videoKey);
+            String videoThumbnailUrl = fileUploadService.uploadFile(thumbnail, thumbnailKey);
+
+            log.info("성공적으로 비디오가 업로드되었습니다. memberId: {}", memberId);
+            return new UploadVideoResponse(uploadedVideoUrl, videoThumbnailUrl);
+        } catch (IllegalArgumentException e) {
+            log.warn("잘못된 형식의 비디오입니다. file: {} for memberId: {}", videoFile.getOriginalFilename(), memberId);
+            throw e;
+        } catch (IOException e) {
+            log.error("비디오 업로드 중 에러가 발생하였습니다. for member: {}", memberId, e);
+            throw new RuntimeException("비디오 파일 업로드 중에 에러가 발생하였습니다.", e);
+        }
+    }
+
+    private String buildKey(MultipartFile file, String additionalDir, String type) {
+        String dirName = type + "/" + additionalDir;
+        return fileUploadService.buildKey(file, dirName);
+    }
+
 }

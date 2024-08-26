e69cc41a2c8f1534d3164f544a6d778983ff1d4a
fakerdeft <whakswp123@naver.com>
2024-08-26 23:26:57 +0900

    (#33)feat: 일정 not found 예외 구현

--- src/main/java/com/adregamdi/schedule/exception/ScheduleException.java
+++ src/main/java/com/adregamdi/schedule/exception/ScheduleException.java
@@ -1,4 +1,17 @@
 package com.adregamdi.schedule.exception;
 
 public class ScheduleException extends RuntimeException {
+    public ScheduleException(final String message) {
+        super(message);
+    }
+
+    public static class ScheduleNotFoundException extends ScheduleException {
+        public ScheduleNotFoundException() {
+            super("일정이 존재하지 않습니다.");
+        }
+
+        public ScheduleNotFoundException(final Object object) {
+            super(String.format("일정이 존재하지 않습니다. - request info => %s", object));
+        }
+    }
 }

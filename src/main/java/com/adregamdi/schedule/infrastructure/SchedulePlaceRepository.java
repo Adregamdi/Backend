7687af98f8baeeba73764f039f3be7f2d46fbc1e
fakerdeft <whakswp123@naver.com>
2024-08-29 02:52:56 +0900

    (#33)fix: optional 삭제

--- src/main/java/com/adregamdi/schedule/infrastructure/SchedulePlaceRepository.java
+++ src/main/java/com/adregamdi/schedule/infrastructure/SchedulePlaceRepository.java
@@ -9,5 +9,5 @@ import java.util.Optional;
 public interface SchedulePlaceRepository extends JpaRepository<SchedulePlace, Long>, ScheduleCustomRepository {
     Optional<List<SchedulePlace>> findByScheduleId(Long scheduleId);
 
-    Optional<SchedulePlace> findByScheduleIdAndPlaceOrder(Long scheduleId, Integer order);
+    SchedulePlace findByScheduleIdAndPlaceOrder(Long scheduleId, Integer order);
 }

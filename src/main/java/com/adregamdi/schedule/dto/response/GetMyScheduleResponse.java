2920f2db3f456d1bd5a7a47e29f3e7189e4b5c73
fakerdeft <whakswp123@naver.com>
2024-08-29 01:33:25 +0900

    (#33)feat: 일정 조회 응답 구현

--- src/main/java/com/adregamdi/schedule/dto/response/GetMyScheduleResponse.java
+++ src/main/java/com/adregamdi/schedule/dto/response/GetMyScheduleResponse.java
@@ -1,4 +1,34 @@
 package com.adregamdi.schedule.dto.response;
 
-public record GetMyScheduleResponse() {
+import com.adregamdi.schedule.domain.Schedule;
+import com.adregamdi.schedule.domain.SchedulePlace;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Builder
+public record GetMyScheduleResponse(
+        List<ScheduleDTO> schedules
+) {
+    public static GetMyScheduleResponse from(List<Schedule> schedules, List<List<SchedulePlace>> schedulePlaces) {
+        List<ScheduleDTO> scheduleDTOs = IntStream.range(0, schedules.size())
+                .mapToObj(i -> new ScheduleDTO(schedules.get(i), schedulePlaces.get(i)))
+                .collect(Collectors.toList());
+
+        return GetMyScheduleResponse.builder()
+                .schedules(scheduleDTOs)
+                .build();
+    }
+
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ScheduleDTO {
+        Schedule schedule;
+        List<SchedulePlace> schedulePlaces;
+    }
 }

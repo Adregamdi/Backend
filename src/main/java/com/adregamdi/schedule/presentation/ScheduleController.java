6768108f0f28cc05344144f264bd207695bda3f0
fakerdeft <whakswp123@naver.com>
2024-08-29 00:29:54 +0900

    (#33)refactor: 일정 조회 컨트롤러 파라미터 수정

--- src/main/java/com/adregamdi/schedule/presentation/ScheduleController.java
+++ src/main/java/com/adregamdi/schedule/presentation/ScheduleController.java
@@ -4,13 +4,17 @@ import com.adregamdi.core.annotation.MemberAuthorize;
 import com.adregamdi.core.handler.ApiResponse;
 import com.adregamdi.schedule.application.ScheduleService;
 import com.adregamdi.schedule.dto.request.CreateMyScheduleRequest;
+import com.adregamdi.schedule.dto.request.GetMyScheduleRequest;
 import com.adregamdi.schedule.dto.response.GetMyScheduleResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/schedule")
@@ -20,11 +24,14 @@ public class ScheduleController {
 
     @GetMapping
     @MemberAuthorize
-    public ResponseEntity<ApiResponse<GetMyScheduleResponse>> getMySchedule(@AuthenticationPrincipal final UserDetails userDetails) {
+    public ResponseEntity<ApiResponse<GetMyScheduleResponse>> getMySchedule(
+            @RequestBody @Valid List<GetMyScheduleRequest> requests,
+            @AuthenticationPrincipal final UserDetails userDetails
+    ) {
         return ResponseEntity.ok()
                 .body(ApiResponse.<GetMyScheduleResponse>builder()
                         .statusCode(HttpStatus.OK.value())
-                        .data(scheduleService.getMySchedule(userDetails.getUsername()))
+                        .data(scheduleService.getMySchedule(requests, userDetails.getUsername()))
                         .build()
                 );
     }
@@ -32,7 +39,7 @@ public class ScheduleController {
     @PostMapping
     @MemberAuthorize
     public ResponseEntity<ApiResponse<Void>> createMySchedule(
-            @RequestBody final CreateMyScheduleRequest request,
+            @RequestBody @Valid final CreateMyScheduleRequest request,
             @AuthenticationPrincipal final UserDetails userDetails
     ) {
         scheduleService.createMySchedule(request, userDetails.getUsername());

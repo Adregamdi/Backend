cb404a6757319bddc6f22281d7d0b48077de76f0
fakerdeft <whakswp123@naver.com>
2024-08-30 02:51:01 +0900

    (#46)feat: 여행기 등록, 조회 API 컨트롤러 구현

---
+++ src/main/java/com/adregamdi/travelogue/presentation/TravelogueController.java
@@ -0,0 +1,47 @@
+package com.adregamdi.travelogue.presentation;
+
+import com.adregamdi.core.annotation.MemberAuthorize;
+import com.adregamdi.core.handler.ApiResponse;
+import com.adregamdi.travelogue.application.TravelogueService;
+import com.adregamdi.travelogue.dto.request.CreateMyTravelogueRequest;
+import com.adregamdi.travelogue.dto.response.GetTravelogueResponse;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/travelogue")
+@RestController
+public class TravelogueController {
+    private final TravelogueService travelogueService;
+
+    @PostMapping
+    @MemberAuthorize
+    public ResponseEntity<ApiResponse<Void>> createMyTravelogue(
+            @RequestBody @Valid final CreateMyTravelogueRequest request,
+            @AuthenticationPrincipal final UserDetails userDetails
+    ) {
+        travelogueService.createMyTravelogue(request, userDetails.getUsername());
+        return ResponseEntity.ok()
+                .body(ApiResponse.<Void>builder()
+                        .statusCode(HttpStatus.CREATED.value())
+                        .build()
+                );
+    }
+
+    @GetMapping
+    @MemberAuthorize
+    public ResponseEntity<ApiResponse<GetTravelogueResponse>> get(@RequestParam("travelogue_id") @Positive Long travelogueId) {
+        return ResponseEntity.ok()
+                .body(ApiResponse.<GetTravelogueResponse>builder()
+                        .statusCode(HttpStatus.OK.value())
+                        .data(travelogueService.get(travelogueId))
+                        .build()
+                );
+    }
+}

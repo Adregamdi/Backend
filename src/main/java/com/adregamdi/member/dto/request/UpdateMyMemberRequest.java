6364c3d9c9540ffe56b2e5671cb04ad110a95010
fakerdeft <whakswp123@naver.com>
2024-08-29 20:44:20 +0900

    (#40)feat: 회원 정보 수정 요청 구현

--- src/main/java/com/adregamdi/member/dto/request/UpdateMyMemberRequest.java
+++ src/main/java/com/adregamdi/member/dto/request/UpdateMyMemberRequest.java
@@ -1,4 +1,8 @@
 package com.adregamdi.member.dto.request;
 
-public record UpdateMyMemberRequest() {
+public record UpdateMyMemberRequest(
+        String name,
+        String profile,
+        String handle
+) {
 }

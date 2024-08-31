a844f918bfbb499dbf8830c3fcc47aaae23932fd
fakerdeft <whakswp123@naver.com>
2024-08-31 16:26:22 +0900

    (#46)feat: 장소 리뷰 테이블 구현

---
+++ src/main/java/com/adregamdi/place/domain/PlaceReview.java
@@ -0,0 +1,23 @@
+package com.adregamdi.place.domain;
+
+import com.adregamdi.core.entity.BaseTime;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "tbl_place_review")
+public class PlaceReview extends BaseTime {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long placeReviewId;
+    @Column
+    private String memberId; // 회원 id
+    @Column
+    private Long placeId; // 장소 id
+    @Column
+    private String content; // 내용
+}

9ab07ce938c2e7ad867b2a5a806174831ba10996
fakerdeft <whakswp123@naver.com>
2024-09-02 03:26:33 +0900

    (#46)feat: 여행기 레포지토리 생성

---
+++ src/main/java/com/adregamdi/travelogue/infrastructure/TravelogueRepository.java
@@ -0,0 +1,7 @@
+package com.adregamdi.travelogue.infrastructure;
+
+import com.adregamdi.travelogue.domain.Travelogue;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TravelogueRepository extends JpaRepository<Travelogue, Long> {
+}

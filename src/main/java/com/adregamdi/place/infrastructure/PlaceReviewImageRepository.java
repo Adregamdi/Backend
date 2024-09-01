50e752a5a7667149c93acd2b79f73dfb5c87a007
fakerdeft <whakswp123@naver.com>
2024-09-02 03:25:55 +0900

    (#46)feat: 장소리뷰이미지 레포지토리 생성

---
+++ src/main/java/com/adregamdi/place/infrastructure/PlaceReviewImageRepository.java
@@ -0,0 +1,7 @@
+package com.adregamdi.place.infrastructure;
+
+import com.adregamdi.place.domain.PlaceReviewImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlaceReviewImageRepository extends JpaRepository<PlaceReviewImage, Long> {
+}

998b3cf57b4ab5a6ac9f48d33ffbfa0e3ad5ab03
fakerdeft <whakswp123@naver.com>
2024-09-06 23:51:51 +0900

    (#64)feat: 일정 응답dto 구현

--- src/main/java/com/adregamdi/travel/dto/response/GetMyTravelsResponse.java
+++ src/main/java/com/adregamdi/travel/dto/response/GetMyTravelsResponse.java
@@ -1,10 +1,17 @@
 package com.adregamdi.travel.dto.response;
 
-import java.time.LocalDate;
+import com.adregamdi.travel.dto.TravelDTO;
+import lombok.Builder;
 
+import java.util.List;
+
+@Builder
 public record GetMyTravelsResponse(
-        LocalDate startDate,
-        LocalDate endDate,
-        String title
+        List<TravelDTO> travels
 ) {
+    public static GetMyTravelsResponse from(List<TravelDTO> content) {
+        return GetMyTravelsResponse.builder()
+                .travels(content)
+                .build();
+    }
 }

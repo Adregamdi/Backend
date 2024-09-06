7cb901b666efee4f7ebadc081178637daa034e12
fakerdeft <whakswp123@naver.com>
2024-09-06 23:29:48 +0900

    (#64)feat: 내 전체 일정 조회 컨트롤러 구현

--- src/main/java/com/adregamdi/travel/presentation/TravelController.java
+++ src/main/java/com/adregamdi/travel/presentation/TravelController.java
@@ -5,6 +5,7 @@ import com.adregamdi.core.handler.ApiResponse;
 import com.adregamdi.travel.application.TravelService;
 import com.adregamdi.travel.dto.request.CreateMyTravelRequest;
 import com.adregamdi.travel.dto.response.GetMyTravelResponse;
+import com.adregamdi.travel.dto.response.GetMyTravelsResponse;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
@@ -44,6 +45,20 @@ public class TravelController {
                 .body(ApiResponse.<GetMyTravelResponse>builder()
                         .statusCode(HttpStatus.OK.value())
                         .data(travelService.getMyTravel(travelId, userDetails.getUsername()))
+                        .build()
+                );
+    }
+
+    @GetMapping("/list")
+    @MemberAuthorize
+    public ResponseEntity<ApiResponse<GetMyTravelsResponse>> getMyTravels(
+            @RequestParam(defaultValue = "0") final int page,
+            @AuthenticationPrincipal final UserDetails userDetails
+    ) {
+        return ResponseEntity.ok()
+                .body(ApiResponse.<GetMyTravelsResponse>builder()
+                        .statusCode(HttpStatus.OK.value())
+                        .data(travelService.getMyTravels(page, userDetails.getUsername()))
                         .build()
                 );
     }

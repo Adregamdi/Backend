b4a62fad8a45ff22767e67642efa1f7116d5f427
fakerdeft <whakswp123@naver.com>
2024-09-08 16:57:54 +0900

    (#36)feat: 선택 위치 기반 근처 장소 추천 api 컨트롤러 구현

--- src/main/java/com/adregamdi/place/presentation/PlaceController.java
+++ src/main/java/com/adregamdi/place/presentation/PlaceController.java
@@ -7,17 +7,62 @@ import com.adregamdi.place.application.PlaceService;
 import com.adregamdi.place.dto.request.CreatePlaceRequest;
 import com.adregamdi.place.dto.response.GetPlaceResponse;
 import com.adregamdi.place.dto.response.GetPlacesResponse;
+import com.adregamdi.place.dto.response.GetSelectionBasedRecommendationPlacesResponse;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URISyntaxException;
+import java.util.List;
+
+@Validated
 @RequiredArgsConstructor
 @RequestMapping("/api/place")
 @RestController
 public class PlaceController {
     private final PlaceService placeService;
+
+    @PostMapping
+    @MemberAuthorize
+    public ResponseEntity<ApiResponse<Void>> create(@RequestBody final CreatePlaceRequest request) {
+        placeService.create(request);
+        return ResponseEntity.ok()
+                .body(ApiResponse.<Void>builder()
+                        .statusCode(HttpStatus.CREATED.value())
+                        .build()
+                );
+    }
+
+    @PostMapping("/use-api")
+    @AdminAuthorize
+    public ResponseEntity<ApiResponse<Void>> createByAPI() {
+        placeService.createByAPI();
+        return ResponseEntity.ok()
+                .body(ApiResponse.<Void>builder()
+                        .statusCode(HttpStatus.CREATED.value())
+                        .build()
+                );
+    }
+
+    @GetMapping("/selection-based")
+    @MemberAuthorize
+    public ResponseEntity<ApiResponse<List<GetSelectionBasedRecommendationPlacesResponse>>> getSelectionBasedRecommendationPlaces(
+            @RequestParam("latitude") @NotNull @DecimalMin("-90.0") @DecimalMax("90.0") final Double latitude,
+            @RequestParam("longitude") @NotNull @DecimalMin("-180.0") @DecimalMax("180.0") final Double longitude
+    ) throws URISyntaxException {
+        return ResponseEntity.ok()
+                .body(ApiResponse.<List<GetSelectionBasedRecommendationPlacesResponse>>builder()
+                        .statusCode(HttpStatus.OK.value())
+                        .data(placeService.getSelectionBasedRecommendationPlaces(latitude, longitude))
+                        .build()
+                );
+    }
 
     @GetMapping
     @MemberAuthorize
@@ -40,28 +85,6 @@ public class PlaceController {
                 .body(ApiResponse.<GetPlacesResponse>builder()
                         .statusCode(HttpStatus.OK.value())
                         .data(placeService.getPlaces(pageNo, name))
-                        .build()
-                );
-    }
-
-    @PostMapping
-    @MemberAuthorize
-    public ResponseEntity<ApiResponse<Void>> create(@RequestBody final CreatePlaceRequest request) {
-        placeService.create(request);
-        return ResponseEntity.ok()
-                .body(ApiResponse.<Void>builder()
-                        .statusCode(HttpStatus.CREATED.value())
-                        .build()
-                );
-    }
-
-    @PostMapping("/use-api")
-    @AdminAuthorize
-    public ResponseEntity<ApiResponse<Void>> createByAPI() {
-        placeService.createByAPI();
-        return ResponseEntity.ok()
-                .body(ApiResponse.<Void>builder()
-                        .statusCode(HttpStatus.CREATED.value())
                         .build()
                 );
     }

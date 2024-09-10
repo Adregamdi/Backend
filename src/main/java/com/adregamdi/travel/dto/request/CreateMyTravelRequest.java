c09eb60f352cc9fc77ff2f6f23aad2e52586dd81
fakerdeft <whakswp123@naver.com>
2024-09-10 18:46:34 +0900

    (#80)refactor: travelId 추가

--- src/main/java/com/adregamdi/travel/dto/request/CreateMyTravelRequest.java
+++ src/main/java/com/adregamdi/travel/dto/request/CreateMyTravelRequest.java
@@ -8,37 +8,28 @@ import java.time.LocalDate;
 import java.util.List;
 
 public record CreateMyTravelRequest(
+        @Positive
+        Long travelId,
         @NotNull
         LocalDate startDate,
-
         @NotNull
         LocalDate endDate,
         @NotBlank
         String title,
-
-        @NotNull
         List<DayInfo> dayList
 ) {
     public record DayInfo(
-            @NotNull
             LocalDate date,
-            @NotNull
             @Positive
             Integer day,
-
             String memo,
-
-            @NotNull
             List<PlaceInfo> placeList
     ) {
     }
 
     public record PlaceInfo(
-            @NotNull
             @Positive
             Long placeId,
-
-            @NotNull
             @Positive
             Integer placeOrder
     ) {

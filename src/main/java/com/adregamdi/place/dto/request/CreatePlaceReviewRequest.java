49ef8bed652488c83a86d035345ef54a767dc21e
fakerdeft <whakswp123@naver.com>
2024-09-12 00:05:09 +0900

    (#88)fix: 이미지 url 파라미터 추가

--- src/main/java/com/adregamdi/place/dto/request/CreatePlaceReviewRequest.java
+++ src/main/java/com/adregamdi/place/dto/request/CreatePlaceReviewRequest.java
@@ -4,12 +4,19 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
+import java.util.List;
+
 public record CreatePlaceReviewRequest(
         @NotNull
         @Positive
         Long placeId,
         @NotNull
         @NotEmpty
-        String content
+        String content,
+        List<PlaceReviewImageInfo> placeReviewImageInfos
 ) {
+    public record PlaceReviewImageInfo(
+            String url
+    ) {
+    }
 }

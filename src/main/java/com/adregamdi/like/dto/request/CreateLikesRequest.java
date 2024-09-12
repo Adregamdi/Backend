130077b1c632adeab07b7acbc385276ead12069f
wanny <115992753+Subak-Uncle@users.noreply.github.com>
2024-09-12 22:30:25 +0900

    [#94][Refactor] 좋아요 생성 시에 장소/여행기 정규식 조회 구현

--- src/main/java/com/adregamdi/like/dto/request/CreateLikesRequest.java
+++ src/main/java/com/adregamdi/like/dto/request/CreateLikesRequest.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
 
 public record CreateLikesRequest (
-        @Pattern(regexp = "PLACE|TRAVELOGUE", message = "장소 혹은 여행기만 입력 가능합니다.")
+        @Pattern(regexp = "(?i)PLACE|TRAVELOGUE", message = "장소(PLACE) 혹은 여행기(TRAVELOGUE)만 입력 가능합니다.")
         ContentType contentType,
         @Positive(message = "식별 값은 자연수만 가능합니다.")
         Long contentId

3b0516c6e4057a339ee79082b637e3c4c4250a63
fakerdeft <whakswp123@naver.com>
2024-09-14 18:07:16 +0900

    (#89)fix: 파라미터 유효성 검사 추가

--- src/main/java/com/adregamdi/member/dto/request/UpdateMyMemberRequest.java
+++ src/main/java/com/adregamdi/member/dto/request/UpdateMyMemberRequest.java
@@ -1,8 +1,17 @@
 package com.adregamdi.member.dto.request;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
 public record UpdateMyMemberRequest(
+        @NotNull
+        @NotEmpty
         String name,
+        @NotNull
+        @NotEmpty
         String profile,
+        @NotNull
+        @NotEmpty
         String handle
 ) {
 }

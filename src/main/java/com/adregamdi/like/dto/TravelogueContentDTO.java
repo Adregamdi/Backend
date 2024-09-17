da509c05b008137fc6cfe1b4b1836888092011d2
UncleSubak <kko6369@naver.com>
2024-09-17 16:39:27 +0900

    [#102][Refactor] 여행기 컨텐츠 DTO 설정

--- src/main/java/com/adregamdi/like/dto/TravelogueContentDTO.java
+++ src/main/java/com/adregamdi/like/dto/TravelogueContentDTO.java
@@ -5,15 +5,18 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class TravelogueContentDTO {
 
-    private Long scheduleId;
+    private Long travelogueId;
     private String title;
-    private String name;
-    private String profile;
+    private List<String> imageList;
+    private String name; // 작성자 닉네임
+    private String profile; // 작성자 프로필 사진
 
 }

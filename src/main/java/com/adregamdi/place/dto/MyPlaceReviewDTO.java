fad1e41fe8776948a13028613c4b5c2452eb855d
fakerdeft <whakswp123@naver.com>
2024-09-17 21:47:40 +0900

    (#104)feat: 내 리뷰 dto 구현

--- src/main/java/com/adregamdi/place/dto/MyPlaceReviewDTO.java
+++ src/main/java/com/adregamdi/place/dto/MyPlaceReviewDTO.java
@@ -1,10 +1,12 @@
 package com.adregamdi.place.dto;
 
 import com.adregamdi.place.domain.PlaceReviewImage;
+import lombok.Builder;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Builder
 public record MyPlaceReviewDTO(
         String title,
         String contentsLabel,
@@ -16,4 +18,27 @@ public record MyPlaceReviewDTO(
         List<PlaceReviewImage> placeReviewImageList,
         LocalDate createdAt
 ) {
+    public static MyPlaceReviewDTO of(
+            final String title,
+            final String contentsLabel,
+            final String regionLabel,
+            final Integer imageReviewCount,
+            final Integer shortsReviewCount,
+            final String travelDate,
+            final String content,
+            final List<PlaceReviewImage> placeReviewImageList,
+            final LocalDate createdAt
+    ) {
+        return MyPlaceReviewDTO.builder()
+                .title(title)
+                .contentsLabel(contentsLabel)
+                .regionLabel(regionLabel)
+                .imageReviewCount(imageReviewCount)
+                .shortsReviewCount(shortsReviewCount)
+                .travelDate(travelDate)
+                .content(content)
+                .placeReviewImageList(placeReviewImageList)
+                .createdAt(createdAt)
+                .build();
+    }
 }

3e8eee21a89e442dc81d244e42283b52e0d1c90b
fakerdeft <whakswp123@naver.com>
2024-09-19 01:50:09 +0900

    (#106)fix: 내림차순으로 메서드 수정

--- src/main/java/com/adregamdi/place/infrastructure/PlaceReviewImageRepository.java
+++ src/main/java/com/adregamdi/place/infrastructure/PlaceReviewImageRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface PlaceReviewImageRepository extends JpaRepository<PlaceReviewImage, Long> {
-    List<PlaceReviewImage> findByPlaceReviewId(Long placeReviewId);
+    List<PlaceReviewImage> findByPlaceReviewIdOrderByPlaceReviewImageIdDesc(Long placeReviewId);
 }

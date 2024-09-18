565811d32bcc5a65d102bdad1b084856986f6c75
fakerdeft <whakswp123@naver.com>
2024-09-19 00:03:55 +0900

    (#106)refactor: 방문 날짜 필드명 수정

--- src/main/java/com/adregamdi/place/dto/MyPlaceReviewDTO.java
+++ src/main/java/com/adregamdi/place/dto/MyPlaceReviewDTO.java
@@ -8,34 +8,37 @@ import java.util.List;
 
 @Builder
 public record MyPlaceReviewDTO(
+        Long placeReviewId,
         String title,
         String contentsLabel,
         String regionLabel,
         Integer imageReviewCount,
         Integer shortsReviewCount,
-        String travelDate,
+        String visitDate,
         String content,
         List<PlaceReviewImage> placeReviewImageList,
         LocalDate createdAt
 ) {
     public static MyPlaceReviewDTO of(
+            final Long placeReviewId,
             final String title,
             final String contentsLabel,
             final String regionLabel,
             final Integer imageReviewCount,
             final Integer shortsReviewCount,
-            final String travelDate,
+            final String visitDate,
             final String content,
             final List<PlaceReviewImage> placeReviewImageList,
             final LocalDate createdAt
     ) {
         return MyPlaceReviewDTO.builder()
+                .placeReviewId(placeReviewId)
                 .title(title)
                 .contentsLabel(contentsLabel)
                 .regionLabel(regionLabel)
                 .imageReviewCount(imageReviewCount)
                 .shortsReviewCount(shortsReviewCount)
-                .travelDate(travelDate)
+                .visitDate(visitDate)
                 .content(content)
                 .placeReviewImageList(placeReviewImageList)
                 .createdAt(createdAt)

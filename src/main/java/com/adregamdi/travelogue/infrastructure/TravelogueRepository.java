16e211665e2da3b9a37a085b4a12e3425d83d096
fakerdeft <whakswp123@naver.com>
2024-09-18 12:26:14 +0900

    (#105)feat: 일정 id로 조회 메서드 구현

--- src/main/java/com/adregamdi/travelogue/infrastructure/TravelogueRepository.java
+++ src/main/java/com/adregamdi/travelogue/infrastructure/TravelogueRepository.java
@@ -4,4 +4,5 @@ import com.adregamdi.travelogue.domain.Travelogue;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TravelogueRepository extends JpaRepository<Travelogue, Long>, TravelogueCustomRepository {
+    Travelogue findByTravelId(Long travelId);
 }

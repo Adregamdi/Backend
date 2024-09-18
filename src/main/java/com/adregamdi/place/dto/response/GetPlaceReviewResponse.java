41666fe83472a0ab433a63a4625f8f0a6d823d63
fakerdeft <whakswp123@naver.com>
2024-09-19 00:12:41 +0900

    (#106)fix: 특정 장소 리뷰 조회 응답 dto 수정

--- src/main/java/com/adregamdi/place/dto/response/GetPlaceReviewResponse.java
+++ src/main/java/com/adregamdi/place/dto/response/GetPlaceReviewResponse.java
@@ -1,18 +1,38 @@
 package com.adregamdi.place.dto.response;
 
+import com.adregamdi.place.domain.Place;
+import com.adregamdi.place.domain.PlaceReview;
 import com.adregamdi.place.domain.PlaceReviewImage;
+import lombok.Builder;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Builder
 public record GetPlaceReviewResponse(
         Long placeReviewId,
         String title,
         String contentsLabel,
         String regionLabel,
-        String visitDate,
+        LocalDate visitDate,
         String content,
         List<PlaceReviewImage> placeReviewImageList,
         LocalDate createdAt
 ) {
+    public static GetPlaceReviewResponse of(
+            final Place place,
+            final PlaceReview placeReview,
+            final List<PlaceReviewImage> placeReviewImages
+    ) {
+        return GetPlaceReviewResponse.builder()
+                .placeReviewId(placeReview.getPlaceReviewId())
+                .title(place.getTitle())
+                .contentsLabel(place.getContentsLabel())
+                .regionLabel(place.getRegionLabel())
+                .visitDate(placeReview.getVisitDate())
+                .content(placeReview.getContent())
+                .placeReviewImageList(placeReviewImages)
+                .createdAt(LocalDate.from(placeReview.getCreatedAt()))
+                .build();
+    }
 }

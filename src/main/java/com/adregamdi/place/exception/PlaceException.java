660afdb87a16e4a92cf08ed98892b1288af0567d
fakerdeft <whakswp123@naver.com>
2024-09-20 22:30:30 +0900

    (#37)feat: 리뷰 존재 예외 추가

--- src/main/java/com/adregamdi/place/exception/PlaceException.java
+++ src/main/java/com/adregamdi/place/exception/PlaceException.java
@@ -35,6 +35,16 @@ public class PlaceException extends RuntimeException {
         }
     }
 
+    public static class PlaceReviewExistException extends PlaceException {
+        public PlaceReviewExistException() {
+            super("이미 해당 장소에 대한 리뷰가 존재합니다.");
+        }
+
+        public PlaceReviewExistException(final Object object) {
+            super(String.format("이미 해당 장소에 대한 리뷰가 존재합니다. - request info => %s", object));
+        }
+    }
+
     public static class PlaceReviewImageNotFoundException extends PlaceException {
         public PlaceReviewImageNotFoundException() {
             super("장소 리뷰 이미지가 존재하지 않습니다.");

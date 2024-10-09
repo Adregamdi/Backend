bcb5c5cafbfbca850392c14d014960c567e8fedb
fakerdeft <whakswp123@naver.com>
2024-10-09 23:42:42 +0900

    (#217)feat: 특정 회원의 리뷰 전체 삭제 메서드 구현

--- src/main/java/com/adregamdi/place/infrastructure/PlaceReviewRepository.java
+++ src/main/java/com/adregamdi/place/infrastructure/PlaceReviewRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface PlaceReviewRepository extends JpaRepository<PlaceReview, Long> {
-    Optional<List<PlaceReview>> findAllByMemberIdOrderByPlaceReviewIdDesc(String memberId);
+    List<PlaceReview> findAllByMemberIdOrderByPlaceReviewIdDesc(String memberId);
 
     List<PlaceReview> findAllByPlaceIdOrderByPlaceReviewIdDesc(Long placeId);
 

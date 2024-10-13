58ccb4f4efbbad1c231077342363fbc640b02d2d
fakerdeft <whakswp123@naver.com>
2024-10-13 20:47:08 +0900

    (#229)refactor: optional 제거

--- src/main/java/com/adregamdi/travelogue/infrastructure/TravelogueImageRepository.java
+++ src/main/java/com/adregamdi/travelogue/infrastructure/TravelogueImageRepository.java
@@ -4,10 +4,9 @@ import com.adregamdi.travelogue.domain.TravelogueImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface TravelogueImageRepository extends JpaRepository<TravelogueImage, Long> {
-    Optional<List<TravelogueImage>> findByTravelogueId(Long travelogueId);
+    List<TravelogueImage> findByTravelogueId(Long travelogueId);
 
     List<TravelogueImage> findAllByTravelogueId(Long travelogueId);
 }
